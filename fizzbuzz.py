e49174f2caa7e8ea7bc77105a54d0e3d8bf20688
clareaduett <42472762+clareaduett@users.noreply.github.com>
2018-09-16 20:04:05 +0300

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+#Lists
+
+X=[]
+Y=[]
+input('Enter list of X: ')
+input('Enter list of Y: ')
+
+#Defining function
+
+def fizzbuzz(X,Y)
+if ((len(X)+len(Y))%3)==0:
+    print('Fizz')
+
+ elif ((len(X)+len(Y))%5)==0:
+    print('Buzz')
+    elif ((len(X)+len(Y))%((3 and 5)or (len(X)+len(Y))):
+          print('fizzbuzz')
+          else:
+          print('Not applicable)
