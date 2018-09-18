4a396783f3761ef1aa1731577ddcdf54cef77b77
clareaduett <42472762+clareaduett@users.noreply.github.com>
2018-09-18 18:41:24 +0300

    Create list_string_loop2.py

---
+++ list_string_loop2.py
@@ -0,0 +1,16 @@
+#List of Vowels
+def countvowel(s):
+    x=['a','e','i','o','u']
+    s=[' ']
+    for char in x:
+      if char in s:
+        s==x
+        return (y)
+        tuple(y)
+#Remove duplicates and z is a list of duplicates       
+        z=list(set(s))
+#counting duplicates
+        len(z)
+s=input('Enter the word: ')
+
+print ((char(y),len(z))
